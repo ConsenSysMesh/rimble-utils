49b24dd27d74a9b70c22d2e76e69fcf449e6cd5a
Mike Lock <michael.lockwitz@consensys.net>
2019-04-11 16:58:05 -0600

    Initial commit.

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import pkg from "./package.json";
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: "src/main.js",
+    output: {
+      name: "rimbleUtils",
+      file: pkg.browser,
+      format: "umd",
+      external: ["bowser"]
+    },
+    plugins: [resolve(), commonjs()]
+  },
+  {
+    input: "src/main.js",
+    external: ["ms"],
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" }
+    ]
+  }
+];
